60e4428aa3d6e05418c15efc7e788042a7635809
Steve King <steve@dodgestar.com>
2012-06-23 09:02:28 +0100

    Allow multiple build profiles for JavaScript Specify remote scripts to include in the list.json for each build profile

--- src/stages/process-javascript.js
+++ src/stages/process-javascript.js
@@ -1,6 +1,92 @@
 
 "use strict";
 
+var TaskRunner = require('task-runner').TaskRunner;
+var XHR = require('xhrequest');
+var fs = require('fs');
+var uglifyParser = require("uglify-js").parser;
+var uglify = require("uglify-js").uglify;
+
+var remoteCache = {};
+var debugFileTemplate = '{{#scripts}}document.write(decodeURIComponent("%3Cscript%20src%3D%22/js/{{source}}?' + Date.now() + '%22%3E%3C%2Fscript%3E"));\n{{/scripts}}';
+
+function process(sourceDir, targetDir, taskRunner, processOptions) {
+
+   var sourceFiles,
+       jsOutputFile = targetDir + (processOptions.filename || 'scripts.js'),
+       jsIncludeFilter = processOptions.include || ['**.js'],
+       jsRemoteUrls = processOptions.remote || [],
+       jsOutput = '',
+       debugFileList = [];
+
+   // task #1 read the directory contents with the supplied filters
+   taskRunner.push(function(next) {
+      sourceFiles = require('readdir').readSync(sourceDir, jsIncludeFilter);
+      next();
+   }, 'Scan for all JS files');
+
+   // task #2 when the package should include remote scripts, download them (or read them from the cache)
+   taskRunner.push(function(next) {
+      var remoteTaskRunner = new (require('task-runner').TaskRunner);
+      [].concat(jsRemoteUrls).forEach(function(url) {
+         if(remoteCache[url]) return;
+
+         remoteTaskRunner.push(function(next) {
+            XHR(url, {
+               success: function(data) {
+                  remoteCache[url] = {
+                     content: data,
+                     baseName: require('path').basename(url)
+                  };
+                  fs.writeFile(targetDir + remoteCache[url].baseName, data, 'utf-8', next);
+               },
+               error: function(e) { throw new Error(e) }
+            });
+         }, 'Fetching ' + url);
+      });
+      remoteTaskRunner.start(next);
+   }, 'Fetch remote scripts');
+
+   // task #3 when the package should include remote scripts, push them to the top of the output
+   taskRunner.push(function(next) {
+      next([].concat(jsRemoteUrls).forEach(function(url) {
+         jsOutput += remoteCache[url].content + '\n\n';
+         debugFileList.push(remoteCache[url].baseName);
+      }));
+   }, 'Concatenate remote scripts');
+
+   // task #4 read all the local file content into the output file
+   taskRunner.push(function(next) {
+      jsOutput = [jsOutput];
+      sourceFiles.forEach(function(sourceFile) {
+         console.log('    [CONCAT] ' + sourceFile);
+         jsOutput.push(fs.readFileSync(sourceDir + sourceFile, 'utf-8'));
+         debugFileList.push(require('path').basename(sourceFile));
+      });
+      jsOutput = jsOutput.join('\n');
+      fs.writeFile(jsOutputFile, jsOutput, 'utf-8', next);
+   }, 'Concatenate JS files');
+
+   // task #5 write out a debug file list
+   taskRunner.push(function(next) {
+      var scripts = {scripts: []};
+      debugFileList.forEach(function(fileName) {scripts.scripts.push({source: fileName})});
+
+      fs.writeFile(jsOutputFile.replace(/\.js$/, '.debug.js'), require('hogan').compile(debugFileTemplate).render(scripts), 'utf-8', next);
+   }, 'Save debug JS file list to ' + jsOutputFile.replace('.js', '.debug.js'));
+
+   // task #6 minify the output content
+   taskRunner.push(function(next) {
+      var ast;
+
+      ast = uglifyParser.parse(jsOutput);    // parse code and get the initial AST
+      ast = uglify.ast_mangle(ast);          // get a new AST with mangled names
+      ast = uglify.ast_squeeze(ast);         // get an AST with compression optimizations
+
+      fs.writeFile(jsOutputFile.replace(/\.js$/, '.min.js'), uglify.gen_code(ast), 'utf-8', next);
+   }, 'Save minified JS file');
+}
+
 /**
  *
  * @param {String} sourcePath
@@ -10,87 +96,27 @@
  */
 module.exports = function(sourcePath, targetPath, taskRunner, options) {
 
+   var sourceDir = sourcePath + (options.jsSource || 'js').replace(/\/$/, '') + '/';
+   var targetDir = targetPath + (options.jsOutput || 'js').replace(/\/$/, '') + '/';
+
    if(options.processJavascript) {
-      (function() {
-         var fs = require('fs'),
-            sourceDir = sourcePath + (options.jsSource || 'js').replace(/\/$/, '') + '/',
-            outputFile = targetPath + (options.jsOutput || 'js/scripts.js'),
-            minifiedOutputFile = outputFile.replace(/\.js$/, '-min.js'),
-            fileContent = '',
-            sourceFiles,
-            debugFileList = [],
-            dependencies = [],
-            dependencyContent = '',
-            jsIncludeFilter = ['**.js'];
-
-         if(options.jsIncludes) {
-            taskRunner.push(function(next) {
-               fs.readFile(sourcePath + options.jsIncludes, 'utf-8', function(err, data) {
-                  if(err) { throw new Error(err); }
-                  jsIncludeFilter = JSON.parse(data).include;
-                  next();
-               });
-            }, 'Parsing JS filters file');
+      taskRunner.push(function(next) {
+         var builds;
+         try {
+            builds = JSON.parse(fs.readFileSync(sourceDir + 'list.json', 'utf-8'));
+            if(!Array.isArray(builds)) {
+               throw new TypeError('JavaScript list.json must contain an array of configuration objects.');
+            }
+         }
+         catch (e) {
+            console.error('[ERROR] Can not parse list.json for javascript', e);
+            process.exit();
          }
 
-         taskRunner.push(function(next) {
-            sourceFiles = require('readdir').readSync(sourceDir, jsIncludeFilter);
-            next();
-         }, 'Scan for all JS files');
-
-         if(options.remoteJs) {
-            taskRunner.push(function(next) {
-               var remoteTaskRunner = new (require('task-runner').TaskRunner);
-               [].concat(options.remoteJs).forEach(function(url) {
-                  remoteTaskRunner.push(function(next) {
-                     require('xhrequest')(url, {
-                        success: function(data) {
-                           var baseName = require('path').basename(url);
-                           dependencyContent += (dependencies[ baseName ] = data) + "\n\n";
-                           fs.writeFile(targetPath + 'js/' + baseName, data, 'utf-8', next);
-                           debugFileList.push(baseName);
-                        },
-                        error: function(e) {
-                           throw new Error(e)
-                        }
-                     });
-                  }, 'Fetching ' + url);
-               });
-               remoteTaskRunner.start(next);
-            }, 'Fetch remote scripts');
-         }
-
-         taskRunner.push(function(next) {
-            fileContent = [dependencyContent];
-            sourceFiles.forEach(function(sourceFile) {
-               console.log('    [CONCAT] ' + sourceFile);
-               fileContent.push(fs.readFileSync(sourceDir + sourceFile, 'utf-8'));
-               debugFileList.push(require('path').basename(sourceFile));
-            });
-            fileContent = fileContent.join('\n');
-            fs.writeFile(outputFile, fileContent, 'utf-8', next);
-         }, 'Concatenate JS files');
-
-         taskRunner.push(function(next) {
-            var debugFileTemplate = '{{#scripts}}document.write(decodeURIComponent("%3Cscript%20src%3D%22/js/{{source}}?' + Date.now() + '%22%3E%3C%2Fscript%3E"));\n{{/scripts}}';
-            var scripts = {scripts: []};
-            debugFileList.forEach(function(fileName) {scripts.scripts.push({source: fileName})});
-
-            fs.writeFile(outputFile.replace('.js', '.debug.js'), require('hogan').compile(debugFileTemplate).render(scripts), 'utf-8', next);
-         }, 'Save debug JS file list to ' + outputFile.replace('.js', '.debug.js'));
-
-         taskRunner.push(function(next) {
-            var jsp = require("uglify-js").parser,
-               pro = require("uglify-js").uglify,
-               ast;
-
-            ast = jsp.parse(fileContent);   // parse code and get the initial AST
-            ast = pro.ast_mangle(ast);      // get a new AST with mangled names
-            ast = pro.ast_squeeze(ast);     // get an AST with compression optimizations
-
-            fs.writeFile(minifiedOutputFile, pro.gen_code(ast), 'utf-8', next);
-         }, 'Save minified JS file');
-      }());
+         next(builds.forEach(function(build) {
+            process(sourceDir, targetDir, taskRunner, build);
+         }));
+      }, 'Parsing JS filters file');
    }
 
 };
